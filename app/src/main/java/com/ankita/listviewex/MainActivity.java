e903e7e619903a3d6d35928026e61e3616374700
Ankita197 <testsoft197@gmail.com>
2020-01-31 17:32:12 +0530

    initial commit

---
+++ app/src/main/java/com/ankita/listviewex/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ankita.listviewex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
